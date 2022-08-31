d15c8794fc0e5a35ed838f3efc8ac7bb16c58fe6
gvazquez <gvazquez@corunet.com>
2022-08-31 11:26:50 +0200

    Cambios estilos css

---
+++ client/sugus/src/components/common/dropdown/dropdown.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useRef, useState } from 'react';
+import { AiFillCaretDown, AiFillCaretUp } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import './dropdown.scss';
+
+export interface DropdownOption {
+    label: string;
+    path: string;
+}
+
+interface DropdownProps {
+    label: string;
+    options: DropdownOption[];
+}
+
+export const Dropdown = ({ label, options }: DropdownProps) => {
+    const [openSubmenu, setOpenSubmenu] = useState<boolean>(false);
+    const wrapperRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (wrapperRef.current && !wrapperRef.current.contains(event.target as Node)) {
+                setOpenSubmenu(false);
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [wrapperRef]);
+
+    const handleToggleSubmenu = () => {
+        setOpenSubmenu(!openSubmenu);
+    };
+
+    return (
+        <div className='dropdown' ref={wrapperRef}>
+            <div className='dropdown__button' onClick={handleToggleSubmenu}>
+                <span className='dropdown__button__label'>{label}</span>
+                {openSubmenu ? (
+                    <AiFillCaretUp className='dropdown__button__icon' />
+                ) : (
+                    <AiFillCaretDown className='dropdown__button__icon' />
+                )}
+            </div>
+
+            {openSubmenu && (
+                <ul className='dropdown__submenu'>
+                    {options &&
+                        options.map((option: DropdownOption, index: number) => {
+                            return (
+                                <>
+                                    <li key={index} className='dropdown__submenu__item'>
+                                        <Link
+                                            to={option.path}
+                                            className='dropdown__submenu__item__link'
+                                        >
+                                            <span className='dropdown__submenu__item__label'>
+                                                {option.label}
+                                            </span>
+                                        </Link>
+                                    </li>
+                                    {options && options.length > 1 ? (
+                                        <span className='dropdown__submenu__item__separation'></span>
+                                    ) : null}
+                                </>
+                            );
+                        })}
+                </ul>
+            )}
+        </div>
+    );
+};
--- client/sugus/src/components/common/slider/slider.tsx
+++ client/sugus/src/components/common/slider/slider.tsx
@@ -1,49 +1,105 @@
 import { Carousel } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './slider.scss';
 
 export const Slider = () => {
     return (
         <Carousel className='slider'>
             <Carousel.Item>
-                <a href='https://su2foundation.org/' target='_blank' rel='noreferrer'>
-                    <img className='d-block' src='/assets/logoSU2.png' alt='Home' />
-                </a>
-                <Carousel.Caption>
-                    <h3>SU2</h3>
-                    <p>SU2 Foundation Page</p>
-                </Carousel.Caption>
+                <div className='slider__item'>
+                    <img
+                        className='slider__item__img first-item'
+                        src='/assets/logoSU2.png'
+                        alt='Home'
+                    />
+
+                    <Carousel.Caption>
+                        <div className='slider__item__caption'>
+                            <h1 className='slider__item__caption__title '>SU2</h1>
+                            <p className='slider__item__caption__description'>
+                                SU2 is a suite of open-source software tools written in C++ for the
+                                numerical solution of partial differential equations (PDE) and
+                                performing PDE-constrained optimization. The primary applications
+                                are computational fluid dynamics and aerodynamic shape optimization,
+                                but has been extended to treat more general equations such as
+                                electrodynamics and chemically reacting flows. SU2 supports
+                                continuous and discrete adjoint for calculating the
+                                sensitivities/gradients of a scalar field.
+                            </p>
+                            <a
+                                href='https://su2foundation.org/'
+                                className='slider__item__caption__button'
+                                target='_blank'
+                                rel='noreferrer'
+                            >
+                                <span>More information</span>
+                            </a>
+                        </div>
+                    </Carousel.Caption>
+                </div>
             </Carousel.Item>
+
             <Carousel.Item>
-                <NavLink to='/server/connection'>
-                    <img className='d-block' src='/assets/wifi.png' alt='Tranfer file' />
-                </NavLink>
-                <Carousel.Caption>
-                    <h3>Connection</h3>
-                    <p>Manage server connection</p>
-                </Carousel.Caption>
+                <div className='slider__item'>
+                    <img
+                        className='slider__item__img second-item'
+                        src='/assets/wifi.png'
+                        alt='Home'
+                    />
+
+                    <Carousel.Caption>
+                        <div className='slider__item__caption'>
+                            <h1 className='slider__item__caption__title'>Connection</h1>
+                            <p className='slider__item__caption__description'>
+                                Manage server connection Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Natus, reiciendis.
+                            </p>
+                            <Link to='/server/connection' className='slider__item__caption__button'>
+                                <span>Set Connection</span>
+                            </Link>
+                        </div>
+                    </Carousel.Caption>
+                </div>
             </Carousel.Item>
+
             <Carousel.Item>
                 <NavLink to='/server/messages'>
-                    <img className='d-block' src='/assets/transfer.png' alt='Tranfer file' />
+                    <img
+                        className='d-block'
+                        style={{ height: '44rem' }}
+                        src='/assets/transfer.png'
+                        alt='Tranfer file'
+                    />
                 </NavLink>
                 <Carousel.Caption>
                     <h3>Send file to server</h3>
                     <p>Send file and launch SU2</p>
                 </Carousel.Caption>
             </Carousel.Item>
+
             <Carousel.Item>
                 <NavLink to='/wizard/create'>
-                    <img className='d-block' src='/assets/wizard.png' alt='Create file' />
+                    <img
+                        className='d-block'
+                        style={{ height: '44rem' }}
+                        src='/assets/wizard.png'
+                        alt='Create file'
+                    />
                 </NavLink>
                 <Carousel.Caption>
                     <h3>Create a config file</h3>
                     <p>Creates a new config file helped by a wizard</p>
                 </Carousel.Caption>
             </Carousel.Item>
+
             <Carousel.Item>
                 <NavLink to='/wizard/continue'>
-                    <img className='d-block' src='/assets/save.png' alt='Create file' />
+                    <img
+                        className='d-block'
+                        style={{ height: '44rem' }}
+                        src='/assets/save.png'
+                        alt='Create file'
+                    />
                 </NavLink>
                 <Carousel.Caption>
                     <h3>Upload file</h3>
