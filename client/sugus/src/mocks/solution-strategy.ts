5d94367b3dddc0318ccddccf18817b9440c366ee
rsanfer <rsanchez@corunet.com>
2022-09-01 21:48:16 +0200

    Improve solution strategy

--- client/sugus/src/mocks/solution-strategy.ts
+++ client/sugus/src/mocks/solution-strategy.ts
@@ -7,7 +7,7 @@ export const SOLUTION_STRATEGY: QuestionFamily = {
     groups: [
         {
             key: 'flow-numerics',
-            title: 'flow-numerics',
+            title: 'Flow numerics',
             type: QuestionType.group,
             visibleWhen: {
                 or: [
@@ -20,43 +20,58 @@ export const SOLUTION_STRATEGY: QuestionFamily = {
             children: [
                 {
                     key: 'NUM_METHOD_GRAD',
-                    type: QuestionType.text,
-                    title: 'NUM_METHOD_GRAD',
+                    type: QuestionType.buttonList,
+                    title: 'Numerical method for spatial gradients',
+                    options: [
+                        { label: 'Green-Gauss theorem', value: 'GREEN_GAUSS' },
+                        { label: 'Unweighted least squares', value: 'LEAST_SQUARES' },
+                        {
+                            label: 'Inverse-distance weighted least squares',
+                            value: 'WEIGHTED_LEAST_SQUARES',
+                        },
+                    ],
                 },
                 {
                     key: 'CFL_NUMBER',
                     type: QuestionType.numeric,
-                    title: 'CFL_NUMBER',
+                    title: 'CFL Number',
+                    default: 15,
                 },
                 {
                     key: 'CFL_ADAPT',
                     type: QuestionType.buttonList,
-                    title: 'CFL_ADAPT',
+                    title: 'Adaptive CFL',
                     options: [
-                        { label: 'YES', value: 'YES' },
-                        { label: 'NO', value: 'NO' },
+                        { label: 'Yes', value: 'YES' },
+                        { label: 'No', value: 'NO' },
                     ],
                 },
                 {
                     key: 'CFL_ADAPT_PARAM',
                     type: QuestionType.text,
-                    title: 'CFL_ADAPT_PARAM',
+                    title: 'Adaptation parameters',
+                    visibleWhen: { '==': [{ var: 'CFL_ADAPT' }, 'YES'] },
+                    description: 'Example: ( 0.1, 2.0, 50.0, 1e10 )',
+                    default: '( 0.1, 2.0, 50.0, 1e10 )',
                 },
                 {
                     key: 'MAX_DELTA_TIME',
                     type: QuestionType.text,
-                    title: 'MAX_DELTA_TIME',
+                    title: 'Maximum local dt in local time stepping simulations',
+                    default: 1e6,
                 },
                 {
                     key: 'RK_ALPHA_COEFF',
                     type: QuestionType.text,
-                    title: 'RK_ALPHA_COEFF',
+                    title: 'Runge-Kutta alpha coefficients',
+                    description: 'Default: ( 0.66667, 0.66667, 1.000000 )',
+                    default: '( 0.66667, 0.66667, 1.000000 )',
                 },
             ],
         },
         {
             key: 'slope-limiter-definition',
-            title: 'slope-limiter-definition',
+            title: 'Slope limiter',
             type: QuestionType.group,
             visibleWhen: {
                 or: [{ '==': [{ var: 'SOLVER' }, 'EULER'] }, { '==': [{ var: 'SOLVER' }, 'RANS'] }],
@@ -65,13 +80,14 @@ export const SOLUTION_STRATEGY: QuestionFamily = {
                 {
                     key: 'VENKAT_LIMITER_COEFF',
                     type: QuestionType.numeric,
-                    title: 'VENKAT_LIMITER_COEFF',
+                    title: "Coefficient for the Venkat's limiter",
+                    default: 0.1,
                 },
             ],
         },
         {
             key: 'multigrid-strategy',
-            title: 'multigrid-strategy',
+            title: 'Multi-grid strategy',
             type: QuestionType.group,
             visibleWhen: {
                 or: [
@@ -85,54 +101,65 @@ export const SOLUTION_STRATEGY: QuestionFamily = {
                 {
                     key: 'MGLEVEL',
                     type: QuestionType.numeric,
-                    title: 'MGLEVEL',
+                    title: 'Multigrid level',
+                    default: 0,
                 },
             ],
         },
 
         {
             key: 'with-mglevel',
-            title: 'with-mglevel',
+            title: 'Type of multi-grid',
             type: QuestionType.group,
             visibleWhen: {
-                '!==': [{ var: 'MGLEVEL' }, '0'],
+                '!==': [{ var: 'MGLEVEL' }, 0],
             },
             children: [
                 {
                     key: 'MGCYCLE',
-                    type: QuestionType.numeric,
-                    title: 'MGCYCLE',
+                    type: QuestionType.buttonList,
+                    title: 'Multi-grid cycle',
+                    options: [
+                        { label: 'V cycle', value: 'V_CYCLE' },
+                        { label: 'W cycle', value: 'W_CYCLE' },
+                        { label: 'FullMG cycle.', value: 'FULLMG_CYCLE' },
+                    ],
                 },
                 {
                     key: 'MG_PRE_SMOOTH',
                     type: QuestionType.text,
-                    title: 'MG_PRE_SMOOTH',
+                    title: 'Multi-grid pre-smoothing level',
+                    default: '( 1, 2, 3, 3 )',
                 },
                 {
                     key: 'MG_POST_SMOOTH',
                     type: QuestionType.text,
-                    title: 'MG_POST_SMOOTH',
+                    title: 'Multi-grid post-smoothing level',
+                    default: '( 0, 0, 0, 0 )',
                 },
                 {
                     key: 'MG_CORRECTION_SMOOTH',
                     type: QuestionType.text,
-                    title: 'MG_CORRECTION_SMOOTH',
+                    title: 'Multi-grid smoothing correction',
+                    default: '( 0, 0, 0, 0 )',
                 },
                 {
                     key: 'MG_DAMP_RESTRICTION',
                     type: QuestionType.numeric,
-                    title: 'MG_DAMP_RESTRICTION',
+                    title: 'Multi-grid damping restriction',
+                    default: 0.75,
                 },
                 {
                     key: 'MG_DAMP_PROLONGATION',
                     type: QuestionType.numeric,
-                    title: 'MG_DAMP_PROLONGATION',
+                    title: 'Damping factor for the correction prolongation',
+                    default: 0.75,
                 },
             ],
         },
         {
             key: 'flow-convection',
-            title: 'flow-convection',
+            title: 'Convective methods for the flow',
             type: QuestionType.group,
             visibleWhen: {
                 or: [
@@ -145,18 +172,30 @@ export const SOLUTION_STRATEGY: QuestionFamily = {
             children: [
                 {
                     key: 'CONV_NUM_METHOD_FLOW',
-                    type: QuestionType.text,
-                    title: 'CONV_NUM_METHOD_FLOW',
+                    type: QuestionType.buttonList,
+                    title: 'Convective numerical method',
+                    options: [
+                        { label: 'Jameson-Smith-Turkel centered', value: 'JST' },
+                        { label: 'Lax-Friedrich centered', value: 'LAX' },
+                        { label: 'JST with matrix dissipation', value: 'JST_MAT' },
+                        { label: 'Scalar upwind', value: 'SCALAR_UPWIND' },
+                        { label: 'Roe', value: 'ROE' },
+                        { label: 'AUSM', value: 'AUSM' },
+                    ],
                 },
                 {
                     key: 'JST_SENSOR_COEFF',
                     type: QuestionType.text,
-                    title: 'JST_SENSOR_COEFF',
+                    visibleWhen: {
+                        '==': [{ var: 'CONV_NUM_METHOD_FLOW' }, 'JST'],
+                    },
+                    title: '2nd and 4th order artificial dissipation for JST',
+                    default: '( 0.5, 0.02 )',
                 },
                 {
                     key: 'MUSCL_FLOW',
                     type: QuestionType.buttonList,
-                    title: 'MUSCL_FLOW',
+                    title: 'Monotonic Upwind Scheme for Conservation Laws (MUSCL)',
                     options: [
                         { label: 'YES', value: 'YES' },
                         { label: 'NO', value: 'NO' },
@@ -164,33 +203,50 @@ export const SOLUTION_STRATEGY: QuestionFamily = {
                 },
                 {
                     key: 'SLOPE_LIMITER_FLOW',
-                    type: QuestionType.text,
-                    title: 'SLOPE_LIMITER_FLOW',
+                    type: QuestionType.buttonList,
+                    title: 'Slope limiter',
+                    options: [
+                        { label: 'None', value: 'NONE' },
+                        { label: 'VENKATAKRISHNAN', value: 'VENKATAKRISHNAN' },
+                        { label: 'Van-Albada Edge', value: 'VAN_ALBADA_EDGE' },
+                    ],
                 },
                 {
                     key: 'TIME_DISCRE_FLOW',
-                    type: QuestionType.text,
-                    title: 'TIME_DISCRE_FLOW',
+                    type: QuestionType.buttonList,
+                    title: 'Time discretization',
+                    options: [
+                        { label: 'Explicit RK', value: 'RUNGE-KUTTA_EXPLICIT' },
+                        { label: 'Explicit Euler', value: 'EULER_IMPLICIT' },
+                        { label: 'Implicit', value: 'EULER_EXPLICIT' },
+                    ],
                 },
             ],
         },
         {
             key: 'turbulent-convection',
-            title: 'turbulent-convection',
+            title: 'Convective methods for turbulence',
             type: QuestionType.group,
             visibleWhen: {
-                or: [{ '==': [{ var: 'SOLVER' }, 'EULER'] }, { '==': [{ var: 'SOLVER' }, 'RANS'] }],
+                or: [
+                    { '==': [{ var: 'SOLVER' }, 'RANS'] },
+                    { '==': [{ var: 'SOLVER' }, 'INC_RANS'] },
+                ],
             },
             children: [
                 {
                     key: 'CONV_NUM_METHOD_TURB',
-                    type: QuestionType.text,
-                    title: 'CONV_NUM_METHOD_TURB',
+                    type: QuestionType.buttonList,
+                    title: 'Convective numerical method',
+                    options: [
+                        { label: 'Scalar upwind', value: 'SCALAR_UPWIND' },
+                        { label: 'Roe', value: 'ROE' },
+                    ],
                 },
                 {
                     key: 'MUSCL_TURB',
                     type: QuestionType.buttonList,
-                    title: 'MUSCL_TURB',
+                    title: 'Monotonic Upwind Scheme for Conservation Laws (MUSCL)',
                     options: [
                         { label: 'YES', value: 'YES' },
                         { label: 'NO', value: 'NO' },
@@ -198,26 +254,46 @@ export const SOLUTION_STRATEGY: QuestionFamily = {
                 },
                 {
                     key: 'SLOPE_LIMITER_TURB',
-                    type: QuestionType.text,
-                    title: 'SLOPE_LIMITER_TURB',
+                    type: QuestionType.buttonList,
+                    title: 'Slope limiter',
+                    options: [
+                        { label: 'None', value: 'NONE' },
+                        { label: 'VENKATAKRISHNAN', value: 'VENKATAKRISHNAN' },
+                        { label: 'Van-Albada Edge', value: 'VAN_ALBADA_EDGE' },
+                    ],
                 },
                 {
                     key: 'TIME_DISCRE_TURB',
-                    type: QuestionType.text,
-                    title: 'TIME_DISCRE_TURB',
+                    type: QuestionType.buttonList,
+                    title: 'Time discretization',
+                    options: [
+                        { label: 'Explicit RK', value: 'RUNGE-KUTTA_EXPLICIT' },
+                        { label: 'Explicit Euler', value: 'EULER_IMPLICIT' },
+                        { label: 'Implicit', value: 'EULER_EXPLICIT' },
+                    ],
                 },
             ],
         },
         {
             key: 'structural-model',
-            title: 'structural-model',
+            title: 'Structural model',
             type: QuestionType.group,
             visibleWhen: { '==': [{ var: 'SOLVER' }, 'ELASTICITY'] },
             children: [
                 {
                     key: 'NONLINEAR_FEM_SOLUTION_METHOD',
-                    type: QuestionType.text,
-                    title: 'NONLINEAR_FEM_SOLUTION_METHOD',
+                    type: QuestionType.buttonList,
+                    title: 'Non-linear FEM Method',
+                    options: [
+                        { label: 'Newton-Raphson', value: 'NEWTON_RAPHSON' },
+                        { label: 'Modified Newton-Raphson', value: 'MODIFIED_NEWTON_RAPHSON' },
+                    ],
+                },
+                {
+                    key: 'INNER_ITER',
+                    type: QuestionType.numeric,
+                    title: 'Number of iterations per time step',
+                    default: 10,
                 },
             ],
         },
